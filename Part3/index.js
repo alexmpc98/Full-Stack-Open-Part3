b758c0ee945be4dbac339018e4fcd0736c549e7a
Alexandre Coelho <201701820@estudantes.ips.pt>
2022-08-15 20:34:20 +0100

    Part 3-) b-) (1)
    Part 3-) b-) (1)

--- Part3/index.js
+++ Part3/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 const app = express()
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(express.json())
+app.use(cors())
 
 
 let persons = [
@@ -88,7 +91,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
